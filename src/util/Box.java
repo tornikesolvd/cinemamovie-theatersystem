82f1f81b980e670c5f83f322127e012873703dad
Tornike <taladashvili@laba.solvd.com>
2025-09-19 18:02:13 +0400

    commit11

---
+++ src/util/Box.java
@@ -0,0 +1,27 @@
+package util;
+
+public class Box<T> {
+
+    private T value;
+
+    public Box() {
+    }
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public boolean isEmpty() {
+        return value == null;
+    }
+}
+
+
